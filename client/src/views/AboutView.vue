f13b1d82545ce1346de9533e88a85932927d6187
Chris J. <shift16@protonmail.com>
2024-10-06 15:20:21 -0400

    Renamed Client to client

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>This is an h2</h2>
+    <div class="box">
+      Testbox
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Hello, world</h1>
+    <p>Testing</p>
+  </main>
+</template>
